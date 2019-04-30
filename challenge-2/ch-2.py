3634218388f7ee9a6357be76d562d906a6bfd967
danielsto <danielsto@protonmail.com>
2019-04-30 09:18:23 +0200

    Challenge 2 - Solved

--- challenge-2/ch-2.py
+++ challenge-2/ch-2.py
@@ -1,8 +1,27 @@
-import math
 import os
 
-in_file_path = os.path.join(os.path.dirname(__file__), "testInput")
-out_file_path = os.path.join(os.path.dirname(__file__), "testOutput.txt")
+import networkx as nx
+
+in_file_path = os.path.join(os.path.dirname(__file__), "submitInput")
+out_file_path = os.path.join(os.path.dirname(__file__), "submitOutput.txt")
+
+
+def count_graph_paths(graph):
+    list_paths = []
+    for path in nx.all_simple_paths(graph, source='Galactica', target='New Earth'):
+        list_paths.append(path)
+    return len(list_paths)
+
+
+def graph_from_dict(graph_dict):
+    g = nx.DiGraph()
+    # Create the nodes from the dictionary keys
+    g.add_nodes_from(graph_dict.keys())
+    # Loop through the dictionary items to create the edges between nodes
+    for k, v in graph_dict.items():
+        g.add_edges_from([(k, t) for t in v])
+    return g
+
 
 with open(in_file_path, 'r') as infile:
     with open(out_file_path, 'w') as outfile:
@@ -10,13 +29,14 @@ with open(in_file_path, 'r') as infile:
 
         for case in range(cases):
             planets = int(infile.readline())
-            print(planets)
             graph = {}
+
             for i in range(planets):
                 route = infile.readline().split(':')
                 origin = route[0]
                 destinations = route[1].rstrip().split(',')
                 graph[origin] = destinations
-                print(graph)
+                stellar_map = graph_from_dict(graph)
 
-            #outfile.write("Case #" + str(case + 1) + ": " + str(total) + "\n")
+            num_paths = count_graph_paths(stellar_map)
+            outfile.write("Case #" + str(case + 1) + ": " + str(num_paths) + "\n")
